272a0930e3cb9e582cdda2dfdd846efea39b63f7
Zhadyra Shynybayeva <shzhadyra96@gmail.com>
2024-09-10 11:57:38 +0500

    sout changed

--- src/main/java/kz/shynybayeva/Main.java
+++ src/main/java/kz/shynybayeva/Main.java
@@ -5,10 +5,9 @@ public class Main {
     private static String msg = "hihihi!";
 
     public static void main(String[] args) {
-        System.out.println(getMessage());
-        }
-
-        private static String getMessage() {
-        return msg;
-        }
+        System.out.println(msg);
     }
+
+
+}
+
